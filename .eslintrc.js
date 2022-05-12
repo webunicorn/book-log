f070e21131b769eaa312ed95690bafbf6f6d2936
aoakua <aoakua@naver.com>
2022-05-12 16:23:01 +0900

    layout default  페이지 생성 및 eslint 파일명 한 단어일 때도 가능하도록 추가함

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,16 @@ module.exports = {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
   },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+  ],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'no-console': 'off',
+    'vue/no-v-html': 0,
+    'vue/multi-word-component-names': 0,
+  },
 }
